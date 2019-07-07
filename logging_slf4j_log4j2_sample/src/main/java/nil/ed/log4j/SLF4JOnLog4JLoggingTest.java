3ded38f0026c01dee11aeee9e136c1059069b34c
delin10 <lidelin10@outlook.com>
2019-07-07 17:45:45 +0800

    slf4j on log4j使用样例

--- logging_slf4j_log4j2_sample/src/main/java/nil/ed/log4j/SLF4JOnLog4JLoggingTest.java
+++ logging_slf4j_log4j2_sample/src/main/java/nil/ed/log4j/SLF4JOnLog4JLoggingTest.java
@@ -1,4 +1,16 @@
 package nil.ed.log4j;
 
-public class SLF4JOnLog4JTest {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SLF4JOnLog4JLoggingTest {
+    public static void main(String[] args) {
+        Logger logger=LoggerFactory.getLogger(SLF4JOnLog4JLoggingTest.class);
+
+        logger.info("ok");
+        logger.debug("ok");
+        logger.warn("ok");
+        logger.trace("ok");
+        logger.error("ok");
+    }
 }
